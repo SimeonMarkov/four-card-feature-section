659dca2d619f79b81984cd53d9ec1ad32fd0c70e
Simeon Markov <simeon.markov1994@gmail.com>
2026-03-30 16:09:50 +0300

    added scss logic for setting the same bg prop for different topline elements

--- script.js
+++
@@ -1,4 +0,0 @@
-document.querySelectorAll(".card-container-topline").forEach(element => {
-    const color = element.getAttribute("class").match(/bg-[a-z]+/)[0].split("-")[1];
-    element.style.backgroundColor = `var(--${color})`;
-})
